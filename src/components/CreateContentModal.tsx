2bdd7257f2db7f15b4d77f7226dde936fcc936e5
Dhanushkumar13 <dhanushpappu99@gmail.com>
2025-03-26 16:22:05 +0530

    compeleted the v1 version of brainly

--- src/components/CreateContentModal.tsx
+++ src/components/CreateContentModal.tsx
@@ -1,14 +1,42 @@
+import { useRef, useState } from "react";
 import { CrossIcon } from "../icons/CrossIcon";
 import { Button } from "./Button";
 import { Input } from "./Input";
+import axios from "axios";
+import { BACKEND_URL } from "../config";
+
+enum ContentType{
+    Youtube = "youtube",
+    Twitter = 'twitter'
+}
 
 export function CreateContentModal({open, onClose}){
-   
+    const titleRef = useRef<HTMLInputElement>();
+    const linkRef = useRef<HTMLInputElement>();
+    const [type, setType] = useState(ContentType.Youtube);
+
+
+    async function addContent(){
+        const title = titleRef.current?.value;
+        const link = linkRef.current?.value;
+
+        await axios.post(`${BACKEND_URL}/api/v1/content`,{
+            link,
+            type,
+            title
+        },{
+            headers:{
+                "Authorization" : localStorage.getItem("token")
+            }
+        })
+
+        onClose();
+   }
  
     
     return(
         <div>
-            {open && 
+            {open && <div>
                 <div className="w-screen h-screen bg-black fixed top-0 left-0 bg-opacity-80 flex justify-center">
                     <div className="flex flex-col justify-center">
 
@@ -17,15 +45,27 @@ export function CreateContentModal({open, onClose}){
                                 <CrossIcon/>
                             </div>
                             <div>
-                                <Input placeholder='Title'/>
-                                <Input placeholder='Link'/>
+                                <Input reference={titleRef} placeholder='Title'/>
+                                <Input  reference={linkRef} placeholder='Link'/>
+                            </div>
+                            <div>
+                                <h1 className="pl-4">Type</h1>
+                                <div className="flex gap-4 p-4">
+                                    <Button onClick={()=>{
+                                        setType(ContentType.Youtube)
+                                    }} text="Youtube" variant={type === ContentType.Youtube ? "primary" :"secondary"}></Button>
+                                    <Button onClick={()=>{
+                                        setType(ContentType.Twitter)
+                                    }} text="Twitter" variant={type === ContentType.Twitter ? "primary" :"secondary"}></Button>
+                                </div>  
                             </div>
                             <div className="flex justify-center"> 
-                            <Button variant="primary" text="Submit"/>
+                            <Button onClick={addContent} variant="primary" text="Submit"/>
                             </div>
                         </span>
 
                     </div>
+                </div>
                 </div>
             }
         </div>
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,13 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "../components/Button";
 import { Card } from "../components/Card";
 import { CreateContentModal } from "../components/CreateContentModal";
 import { PlusIcon } from "../icons/PlusIcon";
 import { ShareIcon } from "../icons/ShareIcon";
 import { Sidebar } from "../components/SideBar";
+import { useContent } from "../hooks/useContent";
+import axios from "axios";
+import { BACKEND_URL } from "../config";
 
 export default function Dashboard(){
   const [modalOpen, setModalOpen] = useState(false);
+  const {contents, refresh} = useContent();
+  
+  useEffect(()=>{
+    refresh()
+  },[modalOpen])
+
   return( 
     <div className="font-medium">
       <Sidebar/>
@@ -16,15 +25,30 @@ export default function Dashboard(){
         setModalOpen(false);
       }}/>
       <div className="flex justify-end gap-4">
-        <Button variant="secondary" text="Share Brain" startIcon={<ShareIcon/>} />
+        <Button onClick={async ()=>{
+          const response = await axios.post(`${BACKEND_URL}/api/v1/brain/share`,{
+            share: true
+          },{
+            headers:{
+              "Authorization": localStorage.getItem('token')
+            }
+          });
+          const shareUrl = `http://localhost:5713/share${response.data.message}`
+          console.log(response)
+          navigator.clipboard.writeText(shareUrl)
+        }} variant="secondary" text="Share Brain" startIcon={<ShareIcon/>} />
         <Button variant="primary" text="Add Content" startIcon={<PlusIcon/> } onClick={()=>{
           setModalOpen(true);
         }} />  
       </div>
-      <div className="flex gap-4">
-        <Card type="twitter" link="https://x.com/doodlextom/status/1903510608047383015" title="Personal Branding"/>
-        <Card type="youtube" link="https://youtu.be/Lv8BD8xefJs?si=wPtvuoABKuzsoxqi" title="How I Work 10 Hours Daily"/>
-      </div>
+      <div className="flex gap-4 flex-wrap">
+        {contents.map(({type,link,title})=>
+        <Card
+         type={type}
+         link={link}
+         title={title}/>      
+        )}
+        </div>
       </div>
     </div>
   )
